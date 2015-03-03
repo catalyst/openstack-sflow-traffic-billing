996191cf57ea285fd8463b85b4fb14f5159fe7c7
Michael Fincham <michael@hotplate.co.nz>
2015-03-03 16:39:02 +1300

    Update accounting.py

--- accounting.py
+++ accounting.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
+
 """
 Use sFlow flow records to classify billable OpenStack traffic and submit this
 to ceilometer.
